2f0f192ec173ea00c38acbd686e9190609b733ee
slaan <jturbeaux@pm.me>
2022-03-16 11:02:59 +0100

    add tibillet module

---
+++ extra-addons/tibillet/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "TiBillet",
+
+    'summary': """
+        Ticketing, cashless and cooperative network for artists and organisations.
+        """,
+
+    'description': """
+        Ticketing, cashless and cooperative network for artists and organisations.
+    """,
+
+    'author': "TiBillet",
+    'website': "https://www.tibillet.re",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base','account','contacts','membership','membership_extension','membership_variable_period','odoo_api'],
+
+    'data': [],
+
+    'demo': [],
+}
---
+++ extra-addons/tibillet/controllers/controllers.py
@@ -0,0 +1,6 @@
+# TiBillet Connector
+# Ticketing, cashless and cooperative network for artists and organisations.
+# Main Dev: Jonas TURBEAUX <contact@tibillet.re>
+# TiBillet - https://www.tibillet.re
+
+# -*- coding: utf-8 -*-
